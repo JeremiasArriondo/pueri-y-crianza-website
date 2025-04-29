17634dd6e493d320a7a5c1aae7280d291eab76ac
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-04-28 23:13:23 -0300

    chore: update git ignore

---
+++ app/api/mercadopago/route.ts
@@ -0,0 +1,23 @@
+import api, { mercadopago } from "@/lib/api";
+import { Payment } from "mercadopago";
+import { revalidatePath } from "next/cache";
+
+export async function POST(request: Request) {
+  // Obtenemos el cuerpo de la petición que incluye información sobre la notificación
+  const body: { data: { id: string } } = await request.json();
+
+  // Obtenemos el pago
+  const payment = await new Payment(mercadopago).get({ id: body.data.id });
+
+  // Si se aprueba, agregamos el mensaje
+  if (payment.status === "approved") {
+    // Obtenemos los datos
+    await api.message.add({ id: payment.id!, text: payment.metadata.text });
+
+    // Revalidamos la página de inicio para mostrar los datos actualizados
+    revalidatePath("/");
+  }
+
+  // Respondemos con un estado 200 para indicarle que la notificación fue recibida
+  return new Response(null, { status: 200 });
+}
