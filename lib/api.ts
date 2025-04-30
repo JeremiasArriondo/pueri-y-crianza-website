fc178f61f3d3bd16ad4d58cf000a277e35e798bc
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2025-04-29 21:24:51 -0300

    fix: removed unnused code

--- lib/api.ts
+++ lib/api.ts
@@ -1,39 +1,5 @@
-import { MercadoPagoConfig, Preference } from "mercadopago";
-
-interface Message {
-  id: number;
-  text: string;
-}
+import { MercadoPagoConfig } from "mercadopago";
 
 export const mercadopago = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN!
 });
-
-const api = {
-  message: {
-    async add(message: Message): Promise<void> {
-      console.log("ADD ");
-    },
-    async submit(text: Message["text"]) {
-      const preference = await new Preference(mercadopago).create({
-        body: {
-          items: [
-            {
-              id: "message",
-              unit_price: 100,
-              quantity: 1,
-              title: "Mensaje nuevo"
-            }
-          ],
-          metadata: {
-            text
-          }
-        }
-      });
-
-      return preference.init_point;
-    }
-  }
-};
-
-export default api;
